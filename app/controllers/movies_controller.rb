e6ae5a1416c661d742fffdbc22ae1f1b6cc557e8
Somnath <Somnath318@gmail.com>
2015-09-23 13:40:19 -0300

    HW 2 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,83 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+        @all_ratings = Movie.uniq.pluck(:rating)
+	@record = Hash.new()
+	@all_ratings.each do|rating|
+	@record[rating] = true
+	end
+	#session[:id] = params[:id] if params[:id]
+	if(params[:id])
+		session[:id] = params[:id]
+	end
+	#session[:ratings] = params[:ratings] if params[:ratings]
+	if(params[:ratings])
+		session[:ratings] = params[:ratings]
+	end
+#redirect code
+	if((params[:ratings] == nil && session[:ratings] != nil) || (params[:id] == nil && session[:id] != nil))
+		flash.keep		
+		redirect_to movies_path(:id => session[:id], :ratings => session[:ratings])
+	end	
+#ends
+
+	if(session[:ratings])
+		@rtng = session[:ratings].keys
+		if(@rtng)
+			@rtng.each do|rating|
+			@record[rating] = true				
+			end			
+		end
+	end
+    #if(params[:id] == "title_header")
+    if(session[:id] == "title_header")
+	if(@rtng)
+		@movies = Movie.where(rating: @rtng).order(:title)
+		@cls_name1 = "hilite"
+		@all_ratings.each do|rating|
+		@record[rating] = false
+		end
+		@rtng.each do|rating|
+		@record[rating] = true
+		end
+	else
+		@movies = Movie.order(:title)
+		@cls_name1 = "hilite"
+	end
+	#	
+
+    #elsif(params[:id] == "release_date_header")
+    elsif(session[:id] == "release_date_header")
+	if(@rtng)
+		@movies = Movie.where(rating: @rtng).order(:release_date)
+		@cls_name2 = "hilite"		
+		@all_ratings.each do|rating|
+		@record[rating] = false
+		end
+		@rtng.each do|rating|
+		@record[rating] = true
+		end
+	else
+		@movies = Movie.order(:release_date)
+		@cls_name2 = "hilite"
+	end
+	#	
+    #elsif(params[:ratings] == nil)
+    elsif(session[:ratings] == nil)  
+	@movies = Movie.all
+    else
+	#@rtng = params[:ratings].keys
+	@rtng = session[:ratings].keys	
+	@movies = Movie.where(rating: @rtng)
+	@all_ratings.each do|rating|
+	@record[rating] = false
+	end
+	@rtng.each do|rating|
+	@record[rating] = true
+	end
+  end
+	
+    
   end
 
   def new
